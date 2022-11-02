fecadabd24d440580396e3d360cef8f4a0e8ead6
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 12:56:23 +0900

    정올 506번 풀이

---
+++ JunOl/src/Q506/Main.java
@@ -0,0 +1,15 @@
+package Q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int he = 170;
+		double we = 68.6;
+		System.out.println("My height");
+		System.out.printf("%d\n", he);
+		System.out.println("My weight");
+		System.out.printf("%.6f\n", we);
+	}
+
+}
