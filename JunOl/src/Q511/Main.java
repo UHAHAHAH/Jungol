8bd62200a89fd77e942eab194a9cb97e226fee24
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 16:35:03 +0900

    511 문제 풀이

---
+++ JunOl/src/Q511/Main.java
@@ -0,0 +1,15 @@
+package Q511;
+
+public class Main {
+	public static void main(String[] args) {
+		int num1 = 55;
+		int num2 = 10;
+		
+		System.out.println("55 - 10 = "+ (num1-num2));
+		
+		num1 = 2008;
+		num2 = 1999;
+		System.out.println("2008 - 1999 = "+ (num1-num2));
+		
+	}
+}
