208e4c8776cb757139a31be8c9d218c128023d4e
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 13:07:19 +0900

    505 문제 풀이

---
+++ JunOl/src/Q505/Main.java
@@ -0,0 +1,11 @@
+package Q505;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I can program well.");
+		System.out.println("Dreams come true.");
+	}
+
+}
