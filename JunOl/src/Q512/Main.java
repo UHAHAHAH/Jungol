9dfd85e3dff4069d8ac215c95e672deb12f6dc49
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 17:44:54 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q512/Main.java
+++ JunOl/src/Q512/Main.java
@@ -9,3 +9,14 @@ public class Main {
 		System.out.printf("%d * %.6f = %.6f\n", we, gr, sum);
 	}
 }
+
+
+////선생님 풀이
+//public class Main {
+//	public static void main(String[] args) {
+//		int weight = 49;
+//		double ratio = 0.2683;
+//		
+//		System.out.printf("%d * %.6f = %.6f\n", weight, ratio, weight*ratio);
+//	}
+//}
