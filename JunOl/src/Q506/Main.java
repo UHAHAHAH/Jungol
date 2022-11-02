b1c49ecc638cc5905c8586ee5a60df3bb2b314f0
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 13:03:35 +0900

    String 입력 풀이

--- JunOl/src/Q506/Main.java
+++ JunOl/src/Q506/Main.java
@@ -4,11 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str1 = "My height";
 		int he = 170;
+		String str2 = "My weight";
 		double we = 68.6;
-		System.out.println("My height");
-		System.out.printf("%d\n", he);
-		System.out.println("My weight");
+		System.out.println(str1);
+		System.out.println(he);
+		System.out.println(str2);
 		System.out.printf("%.6f\n", we);
 	}
 
