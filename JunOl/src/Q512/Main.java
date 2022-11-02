19fc8ae71632a3d4d02861cf19365222fa4e801f
Administrator <Administrator@User-2022DTGVBV>
2022-11-02 17:05:31 +0900

    512번 문제풀이

---
+++ JunOl/src/Q512/Main.java
@@ -0,0 +1,11 @@
+package Q512;
+
+public class Main {
+	public static void main(String[] args) {
+		int we = 49;
+		double gr = 0.2683;
+		double sum = (double) we * gr;
+		
+		System.out.printf("%d * %.6f = %.6f\n", we, gr, sum);
+	}
+}
