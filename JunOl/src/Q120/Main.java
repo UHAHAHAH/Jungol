0580060256bf5d364672d44fb2ced9e734c684c8
Administrator <Administrator@User-2022DTGVBV>
2022-11-03 14:56:46 +0900

    120번 문제풀이

---
+++ JunOl/src/Q120/Main.java
@@ -0,0 +1,21 @@
+package Q120;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		sc.close();
+		
+		if(n1 < n2) {
+			System.out.println(n2-n1);
+		}
+		else {
+			System.out.println(n1-n2);
+		}	
+	}
+}
