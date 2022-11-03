a98796b1e318ab81792057199823617bbbad644a
Administrator <Administrator@User-2022DTGVBV>
2022-11-03 12:17:08 +0900

    533번 문제풀이

---
+++ JunOl/src/Q533/Main.java
@@ -0,0 +1,31 @@
+package Q533;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.next().charAt(0);
+		int age = sc.nextInt();
+		sc.close();
+		
+		if(c=='M') {
+			if(age >= 18) {
+				System.out.println("MAN");
+			}
+			else if(age < 18) {
+				System.out.println("BOY");
+			}
+		}
+		else {
+			if(age >= 18) {
+				System.out.println("WOMAN");
+			}
+			else if(age < 18) {
+				System.out.println("GIRL");
+			}
+		}
+	}
+}
