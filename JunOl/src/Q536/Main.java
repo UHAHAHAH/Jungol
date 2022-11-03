90a7c6a665bce86db5164e074150980da754d9d3
Administrator <Administrator@User-2022DTGVBV>
2022-11-03 17:45:50 +0900

    536번 선생님 풀이 추가

--- JunOl/src/Q536/Main.java
+++ JunOl/src/Q536/Main.java
@@ -10,12 +10,33 @@ public class Main {
 	}
 }
 
-///* for 문 */
+///* for 문 (선생님 풀이와 동일) */
 //public class Main {
 //	public static void main(String[] args) {
 //		int i = 1;
 //		for(i=1;i<=15;i++) {
 //			System.out.printf("%d ",i);
 //		}
+//	}
+//}
+
+/*선생님 풀이*/
+//// 2. while loop
+//public class Main {
+//	public static void main(String[] args) {
+//		int j = 1;
+//		while(j<=15) {
+//			System.out.printf("%d ",j++);
+//		}
+//	}
+//}
+
+//// 3. do while 문 
+//public class Main {
+//	public static void main(String[] args) {
+//		int k=1;
+//		do {
+//			System.out.print(k+" ");
+//		}while (k++ < 15);
 //	}
 //}
