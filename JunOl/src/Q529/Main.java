7b1e1e48d55fd408ade3348306694991e240149b
Administrator <Administrator@User-2022DTGVBV>
2022-11-03 10:24:52 +0900

    528번 문제풀이

---
+++ JunOl/src/Q529/Main.java
@@ -0,0 +1,21 @@
+package Q529;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int he = sc.nextInt();
+		int we = sc.nextInt();
+		sc.close();
+		
+		int b = we + 100 - he;
+		
+		System.out.println(b);
+		
+		if(b > 0 ) {
+			System.out.println("Obesity");
+		}	
+	}
+}
