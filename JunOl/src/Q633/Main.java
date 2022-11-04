f0daa3773b2b8d2e3aa2445c8014d181eaef70d1
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 17:54:37 +0900

    선생님 풀이 추가

--- JunOl/src/Q633/Main.java
+++ JunOl/src/Q633/Main.java
@@ -18,25 +18,60 @@ public class Main {
 			System.out.printf("number? ");
 			
 			n = scan.nextInt();
+			System.out.println();
 			
 			switch(n) {
 			case 1:
-				System.out.println("\nSeoul\n");
+				System.out.println("Seoul");
 				break;
 			case 2:
-				System.out.println("\nWashington\n");
+				System.out.println("Washington");
 				break;
 			case 3:
-				System.out.println("\nTokyo\n");
+				System.out.println("Tokyo");
 				break;
 			case 4:
-				System.out.println("\nBeijing\n");
+				System.out.println("Beijing");
 				break;
 			default:
-				System.out.println("\nnone\n");
+				System.out.println("none");
 				break;
 			}
+			System.out.println();
 		}
 		scan.close();		
 	}
 }
+
+/* 선생님 풀이 */
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//
+//		int inp;
+//
+//		do {
+//			System.out.println("1. Korea");
+//			System.out.println("2. USA");
+//			System.out.println("3. Japan");
+//			System.out.println("4. China");
+//			System.out.printf("number? ");
+//			inp = scan.nextInt();
+//
+//			System.out.println();
+//			if (inp == 1) {
+//				System.out.println("Seoul");
+//			} else if (inp == 2) {
+//				System.out.println("Washington");
+//			} else if (inp == 3) {
+//				System.out.println("Tokyo");
+//			} else if (inp == 4) {
+//				System.out.println("Beijing");
+//			}
+//			System.out.println();
+//		} while (1 <= inp && inp <= 4);
+//		sc.close();
+//		System.out.println("none");
+//	}
+//}
