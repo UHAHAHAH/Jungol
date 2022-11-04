86f7dfbc8c2ebabce611251fe0a033b2afef2b26
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 11:19:32 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q537/Main.java
+++ JunOl/src/Q537/Main.java
@@ -7,15 +7,49 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt();
+		sc.close();
 		int c = 0;
 		int sum = 0;
-		sc.close();
-		
-		while(c!=n+1) {
-			sum+=c;
+
+		while (c != n + 1) {
+			sum += c;
 			c++;
 		}
-		
+
 		System.out.println(sum);
 	}
 }
+
+///* 선생님 풀이( while 문 ) */
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//
+//		int inp = sc.nextInt();
+//		sc.close();
+//		int sum = 0, j =1;
+//
+//		while (j <= inp )) {
+//			sum += inp++;
+//		}
+//
+//		System.out.println(sum);
+//	}
+//}
+
+///* 선생님 풀이( For 문 ) */
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//
+//		int inp = sc.nextInt();
+//		sc.close();
+//
+//		int sum = 0;
+//
+//		for (int i = 1; i <= inp; i++) {
+//			sum += i;
+//		}
+//		System.out.println(sum);
+//	}
+//}
