898c5b5c096163c7ff8785b3329a95c00ebbd34e
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 13:17:41 +0900

    632번 문제풀이

--- JunOl/src/Q632/Main.java
+++ JunOl/src/Q632/Main.java
@@ -5,15 +5,44 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		
+
 		int n1 = scan.nextInt();
 		int n2 = scan.nextInt();
 		int n3 = scan.nextInt();
 		scan.close();
-		
+
 		int min = (n1 < n2) ? n1 : n2;
 		min = (min < n3) ? min : n3;
-		
-		System.out.println(min);		
+
+		System.out.println(min);
 	}
 }
+
+///* 선생님 풀이 */
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		int num1 = scan.nextInt();
+//		int num2 = scan.nextInt();
+//		int num3 = scan.nextInt();
+//		sc.close();
+//		
+//		if(num1 > num2) {
+//			if(num2 > num3) {
+//				System.out.println(num3);
+//			}
+//			else {
+//				System.out.println(num2);
+//			}
+//		}
+//		else {
+//			if(num1 > num3) {
+//				System.out.println(num3);
+//			}
+//			else {
+//				System.out.println(num1);
+//			}
+//		}	 
+//	}
+//}
