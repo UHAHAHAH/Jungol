65a25cc7cf89913ac06b5b09144418538e3cba9d
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 15:56:48 +0900

    542번 문제풀이

---
+++ JunOl/src/Q542/Main.java
@@ -0,0 +1,10 @@
+package Q542;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=10; i<=20; i++) {
+			System.out.printf("%d ",i);
+		}
+	}
+}
