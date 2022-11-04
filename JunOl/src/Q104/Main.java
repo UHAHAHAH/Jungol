f8da0604f05bccd1cc0e6c56b7915bed26c90cf8
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 15:46:24 +0900

    104번 문제풀이

---
+++ JunOl/src/Q104/Main.java
@@ -0,0 +1,19 @@
+package Q104;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int k =90;
+		int m=80;
+		int e=100;
+		int s=k+e+m;
+		
+		System.out.println("kor "+k);
+		System.out.println("mat "+m);
+		System.out.println("eng "+e);
+		System.out.println("sum "+s);
+		
+	}
+
+}
