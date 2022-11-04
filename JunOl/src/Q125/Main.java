30da805b81901d3de3279c0174ac742ce6381174
Administrator <Administrator@User-2022DTGVBV>
2022-11-04 11:28:33 +0900

    125번 문제풀이

---
+++ JunOl/src/Q125/Main.java
@@ -0,0 +1,18 @@
+package Q125;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		sc.close();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.printf("%d ", i);
+		}
+	}
+}
