d7497324182abaf4789bf03bb08422ed7e11609e
Administrator <Administrator@User-2022DTGVBV>
2022-11-07 14:59:55 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q595/Main.java
+++ JunOl/src/Q595/Main.java
@@ -2,10 +2,29 @@ package Q595;
 
 public class Main {
 	public static void main(String[] args) {
-	char[] str = {'H','o','n','g',' ','G','i','l',' ','D','o','n','g'};
+		char[] str = {'H','o','n','g',' ','G','i','l',' ','D','o','n','g'};
 		
 		for(int j=3; j<7; j++) {
 			System.out.printf("%c", str[j]);
 		}
 	}
 }
+
+//public class Main{
+//	public static void main(String[] args) {
+//		String s = "Hong Gil Dong";
+//		
+//		for(int i=3; i<=6; i++) {
+//			System.out.print(s.charAt(i));
+//		}
+//	}
+//}	
+
+/* 선생님 문제풀이 (나머지풀이는 동일) */
+//public class Main{
+//	public static void main(String[] args) {
+//		String str = "Hong Gil Dong";
+//		
+//		System.out.println(str.substring(3,7));
+//	}
+//}	
