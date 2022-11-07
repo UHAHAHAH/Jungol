3eb6cf5899b66f1910e12ca5e985a7de1b8a56b9
Administrator <Administrator@User-2022DTGVBV>
2022-11-07 14:05:13 +0900

    595번 문제풀이

---
+++ JunOl/src/Q595/Main.java
@@ -0,0 +1,11 @@
+package Q595;
+
+public class Main {
+	public static void main(String[] args) {
+	char[] str = {'H','o','n','g',' ','G','i','l',' ','D','o','n','g'};
+		
+		for(int j=3; j<7; j++) {
+			System.out.printf("%c", str[j]);
+		}
+	}
+}
