92ec53f498945bdc61e73713ee264891509ad03e
Administrator <Administrator@User-2022DTGVBV>
2022-11-07 10:07:57 +0900

    508번 문제풀이

--- JunOl/src/Q508/Main.java
+++ JunOl/src/Q508/Main.java
@@ -8,3 +8,13 @@ public class Main{
 		System.out.printf("%10s%10d%10d","eraser",110,97);
 	} 
 }
+
+/* 선생님 풀이 */
+//public class Main{
+//	public static void main(String[] args) {
+//		System.out.printf("%10s%10s%10s\n","item","count","price");
+//		System.out.printf("%10s%10s%10s\n","pen","20","100");
+//		System.out.printf("%10s%10s%10s\n","note","5","95");
+//		System.out.printf("%10s%10s%10s","eraser","110","97");
+//	} 
+//}
