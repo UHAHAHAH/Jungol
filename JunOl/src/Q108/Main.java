8eedc28e65c34b1130479a28d2a28753fd003ac1
Administrator <Administrator@User-2022DTGVBV>
2022-11-07 10:51:16 +0900

    108번 문제풀이

---
+++ JunOl/src/Q108/Main.java
@@ -0,0 +1,10 @@
+package Q108;
+
+public class Main {
+	public static void main(String[] args) {
+		int i = 50;
+		double j = 100.12;
+		double c = (double) i * j;
+		System.out.printf("%.2f * %d = %.0f", j, i, c);
+	}
+}
