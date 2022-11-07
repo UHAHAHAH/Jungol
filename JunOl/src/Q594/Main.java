d6b8d75600bc22e1dacab13271b4eee03edeeaa7
Administrator <Administrator@User-2022DTGVBV>
2022-11-07 13:13:43 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q594/Main.java
+++ JunOl/src/Q594/Main.java
@@ -14,3 +14,19 @@ public class Main {
 		}
 	}
 }
+
+/* 선생님 문제풀이 */
+//import java.util.Scanner;
+//
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//
+//		String inp = scan.next();
+//		scan.close();
+//
+//		for (int i = 0; i < 2; i++) {
+//			System.out.print(inp + inp);
+//		}
+//	}
+//}
