70ec3266f4b0d35ab4edb89596fd6812b8d3adde
Administrator <Administrator@User-2022DTGVBV>
2022-11-08 11:58:53 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q558/Main.java
+++ JunOl/src/Q558/Main.java
@@ -20,3 +20,22 @@ public class Main {
 		sc.close();
 	}
 }
+
+/* 선생님 문제풀이 */
+
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int[] ar = new int[100];
+//		
+//		int i=0;
+//		do {
+//			ar[i]=sc.nextInt();
+//		}while(ar[i++]!=0);
+//		sc.close();
+//		
+//		for(int j = i-1; j>=0;j--) {
+//			System.out.print(ar[j]+" ");
+//		}		
+//	}
+//}
