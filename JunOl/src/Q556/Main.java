c398c3f4a90b9fa2021b29c2e7c0dfb36786214e
Administrator <Administrator@User-2022DTGVBV>
2022-11-08 09:40:42 +0900

    556번 문제풀이

---
+++ JunOl/src/Q556/Main.java
@@ -0,0 +1,12 @@
+package Q556;
+
+public class Main {
+	public static void main(String[] args) {
+
+		int[] i = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+		for (int j = 0; j < 10; j++) {
+			System.out.print(i[j] + " ");
+		}
+	}
+}
