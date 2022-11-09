0838518299e8ce8ec481ae349ad185855c066b24
Administrator <Administrator@User-2022DTGVBV>
2022-11-09 11:58:02 +0900

    150번 문제풀이

---
+++ JunOl/src/Q150/Main.java
@@ -0,0 +1,20 @@
+package Q150;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		char[] c = new char[10];
+		
+		for(int i=0;i<10;i++) {
+			c[i]=sc.next().charAt(0);
+		}
+		sc.close();
+		
+		for(int i=9;i>=0;i--) {
+			System.out.print(c[i]+" ");
+		}
+	}
+}
