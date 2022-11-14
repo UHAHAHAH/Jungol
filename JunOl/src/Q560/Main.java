85612ec379baed38806462c6a3a1fee07916bfb1
Administrator <Administrator@User-2022DTGVBV>
2022-11-14 11:12:54 +0900

    선생님 문제풀이

--- JunOl/src/Q560/Main.java
+++ JunOl/src/Q560/Main.java
@@ -21,3 +21,24 @@ public class Main {
 		System.out.println(min);
 	}
 }
+
+/* 선생님 문제풀이 */
+
+//public class Main {
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int[] num = new int[10];
+//		for (int i = 0; i < 10; i++) {
+//			num[i] = sc.nextInt();
+//		}
+//		sc.close();
+//
+//		int min = Integer.MAX_VALUE;
+//		for (int i = 0; i < num.length; i++) {
+//			if (min > num[i]) {
+//				min = num[i];
+//			}
+//		}
+//		System.out.println(min);
+//	}
+//}
