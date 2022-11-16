7b67c568bcc009809267734bb211428dc6940fd8
Administrator <Administrator@User-2022DTGVBV>
2022-11-16 15:06:26 +0900

    함수 문제풀이 추가

--- JunOl/src/Q596/Main.java
+++ JunOl/src/Q596/Main.java
@@ -24,3 +24,31 @@ public class Main {
 		}
 	}
 }
+
+/* 함수 풀이 */
+//public class Main {
+//	void inp(String a, int n) {
+//		int c = a.length();
+//
+//		if (n > c) {
+//			for (int i = (c - 1); i >= 0; i--) {
+//				System.out.printf("%c", a.charAt(i));
+//			}
+//		} else {
+//			for (int i = (c - 1); i >= (c - n); i--) {
+//				System.out.printf("%c", a.charAt(i));
+//			}
+//		}
+//	}
+//
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//
+//		String a = sc.next();
+//		int n = sc.nextInt();
+//		sc.close();
+//
+//		Main Big = new Main();
+//		Big.inp(a, n);
+//	}
+//}
