318eea63cd385613b11ced4a5e00cab4668b416d
Administrator <Administrator@User-2022DTGVBV>
2022-11-16 09:53:21 +0900

    선생님 문제풀이 추가

--- JunOl/src/Q573/Main.java
+++ JunOl/src/Q573/Main.java
@@ -3,7 +3,6 @@ package Q573;
 import java.util.Scanner;
 
 public class Main {
-	
 	void Na(int num) {
 		int count=1;
 		for(int i=0;i<num;i++)
@@ -16,6 +15,7 @@ public class Main {
 			System.out.println();
 		}
 	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
@@ -26,3 +26,28 @@ public class Main {
 		Nsum.Na(inp);
 	}
 }
+
+/* 선생님 문제풀이 */
+
+//public class Main{
+//	public void rectangle(int p) {
+//		int c=1;
+//		for(int i=0;i<p;i++)
+//		{
+//			for(int j=0;j<p;j++)
+//			{
+//				System.out.print(c++ + " ");
+//			}
+//			System.out.println();
+//		}
+//	}
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int n = sc.nextInt();
+//		sc.close();
+//		
+//		//System.out.println(n);
+//		Main m = new Main();
+//		m.rectangle(n);
+//	}
+//}
